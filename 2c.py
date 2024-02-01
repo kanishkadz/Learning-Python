48a2d898f4f43deb676780d6408d70f72cfcb2aa
kanishkadz <kanishkaadz@gmail.com>
2024-02-01 14:36:51 +0530

    ok

--- 2c.py
+++ 2c.py
@@ -1,11 +1,11 @@
 ll=int(input("Enter lower limit:"))
 ul=int(input("Enter upper limit:"))
-flag=0
-n=0
-for i in range(ll,ul+1):
-    n=i
-    for j in range(2,i):
-        if n%j==0:
-            flag=flag+1
-    if flag==0:
-        print(n," is prime !!")
+for n in range(ll, ul+1):
+    if n>1:
+        for i in range(2,n):
+            if(i==n):
+                i=n
+                break
+            if(n%i==0):
+                n=n-1
+print("Value of highest prime is:",i)
