3244d0afe721d0b51b326825aba566da0b31fb31
kanishkadz <kanishkaadz@gmail.com>
2024-02-01 15:16:56 +0530

    Create 3b.py

---
+++ 3b.py
@@ -0,0 +1,11 @@
+dic={1:"A", 2:"B", 3:"C"}
+dict=dic.items()
+for i in dict:
+    print(i)
+a=int(input("Enter the key to access:"))
+print("The value of enetered key:",dic[a])
+print(dic.get(1))
+dic1={1:"E", 2:"F", 3:"G"}
+dic.update(dic1)
+print("Value after updating:",dic)
+print("Length of the dictionary:",len(dic))
