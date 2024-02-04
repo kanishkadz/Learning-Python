9c1146ec94c493fea12c57d62a0a9f815627aac0
kanishkadz <kanishkaadz@gmail.com>
2024-02-04 22:27:57 +0530

    ok

--- 4c.py
+++ 4c.py
@@ -1,16 +1,8 @@
-def even(a):
-    if (a%2==0):
-        return 1
-    else:
-        return 0
-    
-    
-
-list=[11,22,33,44,55,66,77,88,99,100]
-s=len(list)
-for i in range(0,s):
-    flag=0
-    n=list[i]
-    flag=even(n)
-    if(flag==1):
-        print(n)
+def double(a):
+    d=a+a 
+    return d 
+  
+n1=int(input("Enter a number:"))
+n2=double(n1)
+l=lambda a,b : a + b
+print("Sum using lambda():",l(n1,n2))
